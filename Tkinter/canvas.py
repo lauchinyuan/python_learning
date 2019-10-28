0d363cd94c5ad99a6f700104c4045b35d1121c74
lauchinyuan <m13112458836@163.com>
2019-10-29 00:17:56 +0800

    Add canvas

---
+++ Tkinter/canvas.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+windows = tk.Tk()  # 注意是Tk
+windows.title('tkinter')
+windows.geometry('100x200')  # 设定窗口大小
+
+
+cv = tk.Canvas(windows, bg='blue', width=150, height=100)  # 画布，用于放置图片图形等
+cv.pack()
+
+# 在画布上放置图片
+image_file = tk.PhotoImage(file)
+
+
+def fun():
+    pass
+
+
+windows.mainloop()  # 主循环
