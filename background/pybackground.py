9acb799880264093cde6d7e33bf4883b2f5d1408
lauchinyuan <m13112458836@163.com>
2019-11-13 13:12:51 +0800

    A first Python script

--- background/pybackground.py
+++ background/pybackground.py
@@ -1,3 +1,5 @@
 #!python3.7  # 指定python版本，此命令适用于windows系统启动器(可以加入-32或-64来指定版本)
 import sys  # 导入系统模块
 print(sys.version.split()[0])  # 显示python版本
+
+
---
+++ learning Python Fifth edition/Python_Start.py
@@ -0,0 +1,6 @@
+# 第一段python脚本
+import sys
+print(sys.platform)  # 显示python平台
+print(2 ** 10)
+x = 'Spam!'
+print(x * 8)
