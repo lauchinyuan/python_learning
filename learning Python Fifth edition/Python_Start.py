e3f9354a7579e498df2f0e96a8ce42b890a41838
lauchinyuan <m13112458836@163.com>
2019-11-15 00:29:34 +0800

    Import and reload file

--- learning Python Fifth edition/Python_Start.py
+++ learning Python Fifth edition/Python_Start.py
@@ -1,6 +1,15 @@
 # 第一段python脚本
 import sys
+import Test_Code  # 导入关联文件
+import Test_Code  # 即使两次import也只显示一次结果
+from importlib import reload  # 用次方次可以实现多次导入文件
+reload(Test_Code)
 print(sys.platform)  # 显示python平台
 print(2 ** 10)
 x = 'Spam!'
 print(x * 8)
+
+
+
+
+
