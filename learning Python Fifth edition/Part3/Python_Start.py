97b72d7f3c354aacb518f87f1964d1f0bdc92ad1
lauchinyuan <m13112458836@163.com>
2019-11-17 13:06:54 +0800

    Change Folder

--- learning Python Fifth edition/Part3/Python_Start.py
+++ learning Python Fifth edition/Part3/Python_Start.py
@@ -2,7 +2,7 @@
 import sys
 # import Test_Code  # 导入关联文件
 import Test_Code  # 即使两次import也只显示一次结果
-from Test_Code import new_title  # 使用from...import 语句进行导入
+from Test_Code import new_title # 使用from...import 语句进行导入
 print(new_title)  # 直接变量调用即可，实质上from...import复制了模块的属性
 # from importlib import reload  # 用此方法可以实现多次导入文件
 # reload(Test_Code)
--- learning Python Fifth edition/Part3/Test_Code.py
+++ learning Python Fifth edition/Part3/Test_Code.py
@@ -1,3 +1,4 @@
 print('Hello Python')
 title = "This is a Python file"
 new_title = "You are right!"
+
