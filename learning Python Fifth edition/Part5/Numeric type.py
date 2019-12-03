a9e428a1a7fba6d2ad832858809f1c8b4de45af1
lauchinyuan <m13112458836@163.com>
2019-12-04 00:48:12 +0800

    Add some code related with integer

--- learning Python Fifth edition/Part5/Numeric type.py
+++ learning Python Fifth edition/Part5/Numeric type.py
@@ -1,3 +1,4 @@
+import math
 # 变量与基础表达式
 a = 3
 b = 4
@@ -17,4 +18,112 @@ print(num)
 print('%e' % num)  # 科学计数法
 print('%4.2f' % num)
 
+# 普通比较和链式比较
+X = 2
+Y = 4
+Z = 6
+print(X < Y < Z)  # 支持链式比较
+print(X < Y and Y < Z)  # 结果同上
+
+print(X < Y > Z)
+print(X < Y and Y > Z)
+
+print(1 < 2 < 3.0 < 4)
+print(1 > 2 > 3.0 > 4)
+print(1 == 2 < 3)  # 此链式比较较为晦涩，但结果和C语言不同
+print(1 == 2 and 2 < 3)  # 同上，注意得到的是False
+
+# 浮点数比特位数有限
+print(1.1 + 2.2 == 3.3)  # 特别注意浮点数的比较，此处返回False
+print(1.1 + 2.2)  # 注意结果为3.3000000000000003
+print(int(1.1) + int(2.2) == int(3.3))  # 经过转换后结果为True
+
+# 除法
+# Python3.X中/表示真除法
+print(10/4)  # 结果为2.5
+print(10/4.0)  # 2.5
+print(10//4)  # 2
+print(10//4.0)  # 2.0
+
+# 除法在Python不同版本中不同，
+# 若需使用整数截断除法，建议使用//
+# 若对于整数需要小数部分的浮点数结果，建议使用float调用将数转换为浮点数
+
+# 向下取整除法VS截断除法
+# //结果为向下取整除法
+m = math.floor(2.5)
+print(m)  # 2
+m = math.floor(-2.5)
+print(m)  # -3
+m = math.trunc(2.5)
+print(m)  # 2
+m = math.trunc(-2.5)
+print(m)  # -2
+
+print(5/2)  # 2.5
+print(5/-2)  # -2.5
+print(5//2)  # 2
+print(5//-2)  # -3
+print(5//2.0)  # 2.5
+print(5//-2.0)  # -3.0
+
+# 使用趋近于0的截断除法
+print(math.trunc(5/2))  # 结果为2，而不是3
+
+# 复数
+print(2 + -3j)  # 相当于2-3j
+print(1j * 1j)  # (-1+0j)
+print(2 + 1j * 3)  # (2+3j)
+print((2 + 1j) * 3)
+
+# 十六进制、二进制与八进制
+print(0o1, 0o20, 0o377)  # 八进制,打印结果为整数
+print(0x01, 0x10, 0xFF)  # 十六进制
+print(0b1, 0b10000, 0b11111111)  # 二进制
+# 以上三者结果皆为1 16 255
+
+print(oct(64), hex(64), bin(64))  # 使用指定数制显示
+print(int('64'), int('100', 8), int('40', 16), int('1000000', 2))  # 通过int()方法将其他进制转换为二进制
+# 这对于文件方式读取的字符串数字来说很有意义
+print(int('0x40', 16), int('0b1000000', 2))  # 也可以加上字面量
+
+# 进制格式化
+print('{0:o},{1:x},{2:b}'.format(64, 64, 64))  # 100,40,1000000
+print('%o,%x,%x,%X' % (64, 64, 255, 255))  # 100,40,ff,FF
+
+X = 0xFFFFFFFFFFFFFFFFFFFFFFF
+print(X)  # 4951760157141521099596496895
+print(oct(X))  # 0o3777777777777777777777777777777
+print(bin(X))  # 0b11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
+
+# 按位操作
+x = 1
+print(x << 2)  # 4
+print(x | 2)  # 3
+print(x & 1)  # 1
+x = 0b0001
+print(bin(x << 2))  # 0b100
+print(bin(x | 0b010))  # 0b11
+
+X = 0xFF
+print(bin(X))
+print(X ^ 0b10101010)  # 亦或，85
+print(bin(X ^ 0b10101010))
+print(int('01010101', 2))  # 85
+print(hex(85))  # 0x55
+
+X = 99
+print(bin(X), X.bit_length(), len(bin(X)) - 2)  # 0b1100011 7 7
+
+# 其他内置数值工具
+# math模块
+print(math.pi, math.e)
+print(math.sin(2 * math.pi / 180))  # 三角函数
+print(math.sqrt(144), math.sqrt(2))
+print(pow(2, 4), 2 ** 4, 2.0 ** 4.0)  # 16.0 16 16.0
+print(abs(-42.0), sum((1, 2, 3, 4)))
+print(min(3, 1, 2, 4), max(3, 1, 2, 4))
+print(math.floor(2.567), math.floor(-2.3))  # 向下取整
+print(round(2.567), round(2.467), round(2.567, 2))  # 四舍五入+指定小数位数
+print('%.1f' % 2.567, '{0:.2f}'.format(2.567))  # 混合格式化输出
 
