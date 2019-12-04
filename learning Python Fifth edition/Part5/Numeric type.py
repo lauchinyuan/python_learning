f690467214655a7d1f57dafdc744e4160cd37ce6
lauchinyuan <m13112458836@163.com>
2019-12-05 00:50:56 +0800

    Add some code related to Decimals, fractions, and floating-point numbers

--- learning Python Fifth edition/Part5/Numeric type.py
+++ learning Python Fifth edition/Part5/Numeric type.py
@@ -1,4 +1,8 @@
 import math
+import random
+import decimal
+from decimal import Decimal
+from fractions import Fraction
 # 变量与基础表达式
 a = 3
 b = 4
@@ -126,4 +130,58 @@ print(min(3, 1, 2, 4), max(3, 1, 2, 4))
 print(math.floor(2.567), math.floor(-2.3))  # 向下取整
 print(round(2.567), round(2.467), round(2.567, 2))  # 四舍五入+指定小数位数
 print('%.1f' % 2.567, '{0:.2f}'.format(2.567))  # 混合格式化输出
+
+print(math.sqrt(144))  # 使用模块计算平方根
+print(144 ** .5)  # 表达式法
+print(pow(144, .5))  # 内置函数法
+
+# random模块
+rd = random.Random()
+print(rd)
+print(random.randint(1, 10))  # 随机整数
+print(random.choice(['China', 'Taiwan', 'Hongkong']))
+suits = ['Heart', 'clubs', 'diamond', 'spades']  # 扑克四色
+random.shuffle(suits)  # 洗牌
+print(suits)
+
+# 其他数值类型
+print(0.1 + 0.1 + 0.1 - 0.3)  # 注意：结果非零，浮点数运算缺乏精确性
+# 可以用小数模块替代
+print(Decimal('0.1') + Decimal('0.1') + Decimal('0.1') - Decimal('0.3'))  # 0.0
+print(Decimal('0.1') + Decimal('0.1') + Decimal('0.1') - Decimal('0.30'))  # 0.00
+
+# 设置全局小数精度
+print(Decimal(1)/Decimal(7))  # 0.1428571428571428571428571429
+decimal.getcontext().prec = 4  # 设置小数位数
+print(Decimal(1)/Decimal(7))  # 0.1429
+
+print(1999 + 1.33)
+pay = decimal.getcontext().prec = 20
+print(Decimal(str(1999 + 1.33)))
+
+# 小数上下文管理器
+print(Decimal('1.00')/Decimal('3.00'))  # 0.333333333333...
+with decimal.localcontext() as ctx:  # 临时改变精度
+    ctx.prec = 2
+    print(Decimal('1.00') / Decimal('3.00'))  # 0.33
+print(Decimal('1.00')/Decimal('3.00'))  # 精度返回原来设置
+
+# 分数类型
+x = Fraction(1, 3)
+y = Fraction(4, 6)
+print(x, y)
+print(x + y)
+print(x - y)
+print(x * y)
+print(Fraction('.25'))  # 1/4
+print(Fraction('1.25'))  # 5/4
+
+# 分数和小数中的数值精度
+a = 1/3.0
+b = 4/6.0
+print(a, b, a + b, a - b, a * b, a / b)
+print((1/3) + (6/12))
+print(Fraction(6, 12))  # 自动化简为1/2
+
+# 分数转换和混用类型
 
